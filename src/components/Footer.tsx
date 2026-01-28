d1d3dcfe84b2882db07d2ecfdcdab875e4478bb7
shivam <shivamk44409@gmail.com>
2026-01-28 11:06:55 +0530

    update the footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className=" bottom-0 w-full bg-gray-800 text-white text-center py-2">
+      Made by Shivam Sharma
+    </div>
+  )
+}
+
+export default Footer
